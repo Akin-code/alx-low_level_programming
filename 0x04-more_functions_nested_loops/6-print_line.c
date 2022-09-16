cdce7a7b6314c546d9158e10bdb79ea24b549155
Damilola <akinpentium@gmail.com>
2022-09-15 19:38:27 -0700

    Print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print straight line n time
+ * @n: accepts integer value
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
