ce7b9c5017ca7c96d4c17c7df1c863d79d48e2d1
Damilola <akinpentium@gmail.com>
2022-09-15 17:21:06 -0700

    print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0-9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	for (i = 48, i <= 58, i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
