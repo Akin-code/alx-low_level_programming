b4d7c48f783c4f282b4c43f8509548e2c28d48e9
Damilola <akinpentium@gmail.com>
2022-09-19 12:36:30 -0700

    Array Print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_half - a function that prints half of a string
+ * puts_half - a function that prints half of any string
  * if odd len, n = (length_of_the_string - 1) / 2
  * @str: input
  * Return: half of input
